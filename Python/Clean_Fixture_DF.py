3aa9e96b6277c3febec3247be29dfdc59e1a30d3
sabzuski <sabzuski@gmail.com>
2021-07-16 18:47:16 -0400

    added expected winner and expected loser columns

--- Python/Clean_Fixture_DF.py
+++ Python/Clean_Fixture_DF.py
@@ -1,11 +1,12 @@
 import pandas as pd
 
 # Cleans the excel fixture file into a data frame that will be used as the basis for this project
-# This is part of the process to create the new season file
+# This is STEP 1 of the process to create the new season file
 
 
 class Fixture_DF:
     def __init__(self):
+        # The file being read needs to be the appropriate season
         self._fixture_list = pd.read_excel(r"C:\Users\sabzu\Documents\All EPL Project Files\Fixtures\Fixtures_2020_2021.xlsx", header=1)
         fixture_list = pd.DataFrame(self._fixture_list)
         try:
@@ -19,7 +20,10 @@ class Fixture_DF:
 
         self.fixture_list_df["Winner"] = None
         self.fixture_list_df["Loser"] = None
+        self.fixture_list_df["xWinner"] = None
+        self.fixture_list_df["xLoser"] = None
         self._win_loss()
+        self._expected_win_loss()
 
         self.team_list = list(self.fixture_list_df.Home.unique())
         self.team_list.sort()
@@ -36,4 +40,14 @@ class Fixture_DF:
                 self.fixture_list_df.loc[i, "Winner"] = "Tie"
                 self.fixture_list_df.loc[i, "Loser"] = "Tie"
 
-
+    def _expected_win_loss(self):
+        for i in range(len(self.fixture_list_df)):
+            if self.fixture_list_df.loc[i, "xG"] > self.fixture_list_df.loc[i, "xG.1"]:
+                self.fixture_list_df.loc[i, "xWinner"] = self.fixture_list_df.loc[i, "Home"]
+                self.fixture_list_df.loc[i, "xLoser"] = self.fixture_list_df.loc[i, "Away"]
+            elif self.fixture_list_df.loc[i, "xG"] < self.fixture_list_df.loc[i, "xG.1"]:
+                self.fixture_list_df.loc[i, "xWinner"] = self.fixture_list_df.loc[i, "Away"]
+                self.fixture_list_df.loc[i, "xLoser"] = self.fixture_list_df.loc[i, "Home"]
+            else:
+                self.fixture_list_df.loc[i, "xWinner"] = "Tie"
+                self.fixture_list_df.loc[i, "xLoser"] = "Tie"
